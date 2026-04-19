6793890adc5491a403714051437f94652b77f821
Willem Aandewiel <Willem@Aandewiel.nl>
2026-04-19 17:07:56 +0200

    refactor(config): reorganize GPIO pins and document hardware constraints
    - Reorganize GPIO pin definitions in logical order
    - Add comments documenting GPIO pins to avoid (flash, PSRAM, bootstrap)
    - Add I2C annotations for TFT SCL/SDA pins

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-/*** Last Changed: 2026-04-18 - 15:49 ***/
+/*** Last Changed: 2026-04-19 - 17:07 ***/
 #include <Arduino.h>
 
 #include "buttonInput.h"
@@ -22,7 +22,7 @@
 #include <string>
 #include <time.h>
 
-const char* PROG_VERSION = "v1.3.0";
+const char* PROG_VERSION = "v1.3.1";
 
 //--- Logging tag
 static const char* logTag = "main";
